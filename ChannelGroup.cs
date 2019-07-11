44367da3e29d906144035eed520dded7d17b65ef
la <v-laujan@microsoft.com>
2019-07-10 17:42:38 -0700

    First commit

---
+++ ChannelGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quickStartPersonalTab
+{
+    public class ChannelGroup
+    {
+        public string Message { get; set; }
+
+        public string GetColor()
+        {
+            Message = "ChannelGroup.cs says: 'You picked";
+            return Message;
+        }
+    }
+}
+
